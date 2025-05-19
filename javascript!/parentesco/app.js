7c7c7089a56b5b35b3854db7eb5d310d320c8ed3
lucasmquinteros <lucasmartiniquinteros@gmail.com>
2025-05-19 15:48:24 -0300

    fechas y parentezco

---
+++ javascript!/parentesco/app.js
@@ -0,0 +1,7 @@
+const $padre = document.querySelector(".padre");
+
+const $hijo = document.querySelector(".hijo");
+
+const $hermano = document.querySelector(".hermano");
+
+console.log($hijo.parentNode);
